67afe5340a311b01a865fa5d83e88ee52ac5a312
blue-putty <65861156+blue-putty@users.noreply.github.com>
2022-02-21 09:04:07 -0500

    allow reading of all cloze

--- parser.py
+++ parser.py
@@ -115,9 +115,9 @@ def process_card(parent):
                     counter = int(cloze[0])
                     cloze = cloze[3:len(cloze)]
 
-                    cloze = f"{{{{c{counter}::{cloze}}}}}"
-                    counter += 1
-                    card += cloze
+                cloze = f"{{{{c{counter}::{cloze}}}}}"
+                counter += 1
+                card += cloze
             elif node.has_attr("class"):
                     if node.get("class")[0] == "notion-text-equation-token":
                         math = node.find("annotation")
